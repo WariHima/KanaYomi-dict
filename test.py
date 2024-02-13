8a3393e445c518a46313a60c2624d2bbd832f0d6
WariHima <147214949+WariHima@users.noreply.github.com>
2024-02-13 17:49:46 +0900

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import pyopenjtalk
-
-pyopenjtalk.update_global_jtalk_with_user_dict("kanayomi-dict.dic")
-
-print(pyopenjtalk.g2p("archive"))
-print(pyopenjtalk.g2p("eye"))
-print(pyopenjtalk.g2p("あーかいぶ"))
-print(pyopenjtalk.g2p("あいこん"))
